efda8216b94ed131c61c18ad6167ee1855c1220a
Joshua Casey <joshuatcasey@gmail.com>
2022-06-19 21:41:49 -0500

    use libdependency/common to figure out new versions

--- dependency/retrieval/go.mod
+++ dependency/retrieval/go.mod
@@ -2,13 +2,11 @@ module github.com/joshuatcasey/bundler/retrieval
 
 go 1.18
 
-require (
-	github.com/Masterminds/semver/v3 v3.1.1
-	github.com/paketo-buildpacks/packit/v2 v2.3.0
-	golang.org/x/exp v0.0.0-20220608143224-64259d1afd70
-	github.com/joshuatcasey/bundler/libdependency/common v0.0.0-20220615212510-d0acf95765c6
-)
+require github.com/joshuatcasey/bundler/libdependency/common v0.0.0-20220620023758-1da154bdb634
 
 require (
 	github.com/BurntSushi/toml v1.1.0 // indirect
+	github.com/Masterminds/semver/v3 v3.1.1 // indirect
+	github.com/paketo-buildpacks/packit/v2 v2.3.0 // indirect
+	golang.org/x/exp v0.0.0-20220613132600-b0d781184e0d // indirect
 )
--- dependency/retrieval/retrieve.go
+++ dependency/retrieval/retrieve.go
@@ -1,14 +1,11 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"os"
 
 	"github.com/joshuatcasey/bundler/libdependency/common"
 )
-
-var id = "yarn"
 
 func main() {
 	buildpackTomlPath := os.Args[1]
@@ -17,21 +14,5 @@ func main() {
 	fmt.Printf("buildpackTomlPath=%s\n", buildpackTomlPath)
 	fmt.Printf("output=%s\n", output)
 
-	retrievalOutput := common.RetrievalOutput{
-		Versions: []string{"1.22.18", "1.22.19"},
-		ID:       id,
-		Name:     "Yarn",
-	}
-
-	bytes, err := json.Marshal(retrievalOutput)
-	if err != nil {
-		panic(err)
-	}
-
-	err = os.WriteFile(output, bytes, os.ModePerm)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println(string(bytes))
+	common.GetNewVersions("yarn", "Yarn", buildpackTomlPath, common.GetReleasesFromGithub("", "yarnpkg", "yarn"), output)
 }
